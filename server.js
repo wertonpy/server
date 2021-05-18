b74a4aaf370e18c887e427dc09ea43d81a826ba6
wertonpy <74378549+wertonpy@users.noreply.github.com>
2021-05-18 18:59:21 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,3 +6,13 @@ const port=process.env.PORT;
 app.listen(port, () => {
 console.log(`starting server at ${port}`);
 });
+
+app.use(express.static("public"));
+app.use(express.json({limit:"1mb"}))
+
+const database=new Datastore("database.db");
+database.loadDatabase();
+
+app.post('/api', (req, res) => {
+database.insert(req.body)
+});
