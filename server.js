fdda7560de92db15ad54573221eb2ee0d517fad6
wertonpy <74378549+wertonpy@users.noreply.github.com>
2021-05-25 11:15:50 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -10,9 +10,12 @@ console.log(`starting server at ${port}`);
 app.use(express.static("public"));
 app.use(express.json({limit:"1mb"}))
 
-const database=new Datastore("database.db");
+const database=new Datastore("database1.db");
 database.loadDatabase();
 
 app.post('/api', (req, res) => {
 database.insert(req.body)
 });
+
+
+
